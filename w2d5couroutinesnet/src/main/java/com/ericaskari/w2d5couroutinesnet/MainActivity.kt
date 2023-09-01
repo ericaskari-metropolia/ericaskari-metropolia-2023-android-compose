499875579e54619d55771ccfb3c150148c78a5bb
ericaskari <ericaskari@gmail.com>
2023-09-01 15:20:28 +0300

    w2d5coroutinesnet added

---
+++ w2d5couroutinesnet/src/main/java/com/ericaskari/w2d5couroutinesnet/MainActivity.kt
@@ -0,0 +1,49 @@
+package com.ericaskari.w2d5couroutinesnet
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import coil.compose.AsyncImage
+import com.ericaskari.w2d5couroutinesnet.ui.theme.FirstComposeAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContent {
+            FirstComposeAppTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+                    Greeting("Android")
+                    AsyncImage(
+                        model = "https://users.metropolia.fi/~jarkkov/folderimage.jpg",
+                        contentDescription = "Image"
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    FirstComposeAppTheme {
+        Greeting("Android")
+    }
+}
